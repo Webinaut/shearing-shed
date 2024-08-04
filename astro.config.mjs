f1ab33c0ad8aae729c6ea8a218771d124372f9ad
Martin <slackgt@bigpond.com>
2024-08-04 16:12:31 +1000

    1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import installedIntegration from "@picocss/pico";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    // 1. Imported from an installed npm package
+    // installedIntegration(),
+  ],
+});
